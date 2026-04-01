5533e751872d17482ba36e63d653820543bf3510
Orfeu <darkarontg@gmail.com>
2026-03-31 22:02:58 -0300

    chore: bootstrap Spring Boot project

---
+++ src/test/java/com/orfeu/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.orfeu.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
